eea6570cac883694f66c2096640d9c3e299f72b6
Hyemm_720 <zzxxc070@gmail.com>
2023-04-16 00:36:31 +0900

    📰 Create svg files

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'footer-bg': '#232629',
+        'footer-text': '#9199A1',
+      },
+    },
   },
   plugins: [],
 };
