cbbbd0dec977dda8ac307bd4a2ba01e762e04874
mmmkb1 <mmmkb1@naver.com>
2023-04-18 01:33:32 +0900

    [feat]: QuestionDetail 초안

---
+++ client/src/data/dummy.jsx
@@ -0,0 +1,22 @@
+const dummy = [
+  {
+    name: '김코딩',
+    profileImage: 'https://i.imgur.com/8Km9tLL.jpg',
+    title: '안녕하세요',
+    content:
+      'contentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent',
+    votes: 0,
+    views: 5,
+    createdAt: '2021-05-01T10:00:00.000Z',
+    modifiedAt: '2021-05-01T10:00:00.000Z',
+    answer: [
+      {
+        id: 1,
+        content:
+          'answeransweransweransweransweransweransweransweransweransweransweransweranswer',
+      },
+    ],
+  },
+];
+
+export default dummy;
