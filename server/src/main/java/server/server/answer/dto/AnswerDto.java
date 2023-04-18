129cd0cc931749031a74ff7c81c3aba5efd9771e
kkyunga <rlaruddk1020@naver.com>
2023-04-18 20:14:16 +0900

    🚜[feat]: answer수정

--- server/src/main/java/server/server/answer/dto/AnswerDto.java
+++ server/src/main/java/server/server/answer/dto/AnswerDto.java
@@ -1,20 +1,17 @@
 package server.server.answer.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 public class AnswerDto {
     @Getter
     @Setter
-    @AllArgsConstructor
     public static class postAnswer {
         private String content;
     }
 
     @Getter
     @Setter
-    @AllArgsConstructor
     public static class patchAnswer {
         private long answerId;
         private String content;
@@ -22,7 +19,6 @@ public class AnswerDto {
 
     @Getter
     @Setter
-    @AllArgsConstructor
     public static class responseAnswer {
         private long answerId;
         private String content;
