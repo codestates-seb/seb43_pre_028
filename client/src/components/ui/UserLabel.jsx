049a9edc3a77b092728081b946abcf128542a267
938938 <938kjh@gmail.com>
2023-04-18 23:53:39 +0900

    💄[Add]: 로그인/회원가입 인풋 컴포넌트 추가

---
+++ client/src/components/ui/UserLabel.jsx
@@ -0,0 +1,21 @@
+// TODO : 에러메세지 표시
+// TODO : 입력된 데이터 처리
+
+// * : 로그인과 회원가입의 인풋창 공통 속성 UI
+import { useState } from 'react';
+
+function UserLabel({ children, inputId, className, errorMsg }) {
+  const [errorMessage, setErrorMessage] = useState('');
+  return (
+    <label htmlFor={inputId} className="flex flex-col my-1">
+      {children}
+      <input
+        id={inputId}
+        className={`border border-slate-400 rounded focus:border-blue-800 focus:outline-0 box-border p-2 font-light text-xs ${className}`}
+      />
+      <p className="font-light text-xs text-red-600">{errorMessage}</p>
+    </label>
+  );
+}
+
+export default UserLabel;
