526b51d811c848d5d527482addd4e4448d4aed6d
938938 <938kjh@gmail.com>
2023-04-19 22:03:25 +0900

    📰[Add]:SignUpForm 파일 생성, SignUp 수정

--- client/src/components/signup/SignUp.jsx
+++ client/src/components/signup/SignUp.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
-import ButtonCard from '../ui/ButtonCard';
-import UserInput from '../ui/UserLabel';
 import { LinkIcon } from './Icons';
 import OAuthPart from '../login/OAuthPart';
 import LeftContents from './LeftContents';
+import SignUpForm from './SignUpForm';
 
 // TODO : 글자크기 제안하기
 function SignUp() {
@@ -13,40 +12,7 @@ function SignUp() {
       <LeftContents />
       <div className="flex flex-col justify-center items-center">
         <OAuthPart where="Sign up" />
-        <form className="flex flex-col h-auto border-solid w-72 border-slate-300 bg-white rounded my-4 p-5 justify-between shadow-xl">
-          <UserInput inputId="name" className="w-60 h-8 mt-1">
-            Display name
-          </UserInput>
-          <UserInput inputId="email" className="w-60 h-8 mt-1">
-            Email
-          </UserInput>
-          <UserInput inputId="password" className="w-60 h-8 mt-1">
-            <p className="flex justify-between items-end w-60">Password</p>
-          </UserInput>
-          <p className="text-xs font-light">
-            Passwords must contain at least eight characters, including at least 1 letter
-            and 1 number.
-          </p>
-          <ButtonCard
-            type="submit"
-            className="w-60 h-10 bg-blue-500 text-white hover:bg-blue-600 my-8"
-          >
-            Sign up
-          </ButtonCard>
-          <p className="text-xs font-light">
-            By clicking “Sign up”, you agree to our{' '}
-            <a href="http://#" className="text-sky-800 hover:text-sky-600">
-              terms of service
-            </a>
-            ,
-            <a href="http://#" className="text-sky-800 hover:text-sky-600">
-              privacy policy and
-            </a>
-            <a href="http://#" className="text-sky-800 hover:text-sky-600">
-              cookie policy
-            </a>
-          </p>
-        </form>
+        <SignUpForm />
         <p className="font-light text-xs my-1 mt-4">
           Already have an account?
           <a
