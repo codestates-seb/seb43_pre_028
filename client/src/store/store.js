2823f93b8bb4b3bd3b7ec7cb3289e912d29a0646
938938 <938kjh@gmail.com>
2023-04-25 22:58:12 +0900

    🔨[Refector]:store - signup slice 추가

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,11 +3,13 @@ import loginSlice from './loginSlice';
 import questionSlice from './questionSlice';
 import createQuestionSlice from './createQuestionSlice';
 import questionDetailSlice from './questionDetailSlice';
+import signupSlice from './signupSlice';
 
 // ! : reducer 안에 생성한 slice를 아래와 같은 방식으로 추가.
 const store = configureStore({
   reducer: {
     login: loginSlice.reducer,
+    signup: signupSlice.reducer,
     question: questionSlice.reducer,
     askQuestion: createQuestionSlice.reducer,
     questionDetail: questionDetailSlice.reducer,
