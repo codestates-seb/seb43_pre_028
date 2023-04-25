3539d5ca42c92d57fb0bb18396a2404052d40dda
938938 <938kjh@gmail.com>
2023-04-26 04:52:40 +0900

    🔨[Refector]:login / signup toolkit 수정

--- client/src/store/loginSlice.js
+++ client/src/store/loginSlice.js
@@ -1,34 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchLogin } from '../api/login';
 
+// * : email, pw 등의 정보는 여기 저장하지 않고 로그인 유무만 다루는 것에 대해서.
 const loginSlice = createSlice({
   name: 'loginSlice',
-  initialState: { email: '', pw: '', token: '', state: false },
+  initialState: { status: false },
   reducers: {
-    setEmail: (state, action) => {
-      state.email = action.payload;
-    },
-    setPW: (state, action) => {
-      state.pw = action.payload;
-    },
-    setToken: (state, action) => {
-      state.token = action.payload;
+    setStatus: (state, action) => {
+      state.status = action.payload;
     },
   },
   extraReducers: builder => {
-    // builder.addCase(loginAPI.pending, (state, action) => {
-    //   // 대기 상태일 때
-    // });
+    builder.addCase(fetchLogin.pending, (state, action) => {
+      // 대기 상태일 때
+    });
     builder.addCase(fetchLogin.fulfilled, (state, action) => {
-      // state.token = action.payload;
-      // sessionStorage.setItem('token', state.token);
-      // sessionStorage.getItem('token')
-      // 여기서? token을? 다루나?
-      state.state = true;
+      sessionStorage.setItem('token', state.token);
+      state.status = true;
     });
     builder.addCase(fetchLogin.rejected, (state, action) => {
-      // state.token = '';
-      state.state = false;
+      state.status = false;
     });
   },
 });
@@ -36,4 +27,4 @@ const loginSlice = createSlice({
 // 201 'create'
 
 export default loginSlice;
-export const { setEmail, setPW, setToken } = loginSlice.actions;
+export const { setStatus } = loginSlice.actions;
--- client/src/store/signupSlice.js
+++ client/src/store/signupSlice.js
@@ -3,18 +3,7 @@ import { fetchSignUP } from '../api/signup';
 
 const signupSlice = createSlice({
   name: 'signupSlice',
-  initialState: { name: '', email: '', pw: '', state: false },
-  reducers: {
-    setName: (state, action) => {
-      state.name = action.payload.email;
-    },
-    setEmail: (state, action) => {
-      state.email = action.payload.email;
-    },
-    setPW: (state, action) => {
-      state.pw = action.payload.pw;
-    },
-  },
+  initialState: { state: false },
   extraReducers: builder => {
     // builder.addCase(fetchSignUP.pending, (state, action) => {
     //   // 대기 상태일 때
@@ -30,4 +19,4 @@ const signupSlice = createSlice({
 });
 
 export default signupSlice;
-export const { setName, setEmail, setPW } = signupSlice.actions;
+// export const { setName, setEmail, setPW } = signupSlice.actions;
