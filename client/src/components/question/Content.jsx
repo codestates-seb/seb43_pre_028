60f06af731272adad97a0ae53851bdb3d1003ea9
rlwjd31 <rlwjd31@naver.com>
2023-04-27 00:39:18 +0900

    🚀[Feat]: pagination 기능 구현

--- client/src/components/question/Content.jsx
+++ client/src/components/question/Content.jsx
@@ -1,31 +1,46 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import FadeLoader from 'react-spinners/FadeLoader';
 
 import ContentItem from './ContentItem';
 import { fetchAllQuestions } from '../../api/question';
 import Pagination from '../ui/Pagination';
+import Loading from '../ui/Loading';
 
 function Content() {
   const { status, questions } = useSelector(state => state.question);
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const dispatch = useDispatch();
+  const questionNumsPerPage = 10;
 
   useEffect(() => {
     dispatch(fetchAllQuestions());
   }, [dispatch]);
 
+  const onPaginationButtonClickHandler = (e, selectedPageIndex) => {
+    setSelectedIndex(selectedPageIndex);
+  };
+
   return (
     <div className="questions-container">
-      {status === 'loading' && (
-        <div className="flex w-full h-[90vh] justify-center items-center bg-red">
-          <FadeLoader className="flex w-full bg-red" />
-        </div>
-      )}
+      {status === 'loading' && <Loading />}
+
       {status === 'succeed' &&
-        questions.map(question => (
-          <ContentItem key={question.questionId} {...question} />
-        ))}
-      <Pagination />
+        questions
+          .slice(
+            selectedIndex * questionNumsPerPage,
+            (selectedIndex + 1) * questionNumsPerPage,
+          )
+          .map(question => <ContentItem key={question.questionId} {...question} />)}
+      {status === 'succeed' && (
+        <Pagination
+          selectedIndex={selectedIndex}
+          questionNumsPerPage={questionNumsPerPage}
+          onPaginationButtonClickHandler={onPaginationButtonClickHandler}
+          buttons={Array(Math.ceil(questions.length / questionNumsPerPage))
+            .fill(0)
+            .map((_, index) => index)}
+        />
+      )}
     </div>
   );
 }
--- client/src/components/ui/Pagination.jsx
+++ client/src/components/ui/Pagination.jsx
@@ -1,36 +1,18 @@
-function Pagination({ questions }) {
+function Pagination({ selectedIndex, onPaginationButtonClickHandler, buttons }) {
   return (
     <div className="flex justify-center items-center gap-2 mt-10">
-      <button
-        type="button"
-        className="w-[26px] h-[26px] rounded-sm border-[1px] boder-solid border-[#525960]/20 text-[13px] bg-[#f48225] text-white"
-      >
-        1
-      </button>
-      <button
-        type="button"
-        className="w-[26px] h-[26px] rounded-sm border-[1px] boder-solid border-[#525960]/20 text-[13px] "
-      >
-        2
-      </button>
-      <button
-        type="button"
-        className="w-[26px] h-[26px] rounded-sm border-[1px] boder-solid border-[#525960]/20 text-[13px] "
-      >
-        3
-      </button>
-      <button
-        type="button"
-        className="w-[26px] h-[26px] rounded-sm border-[1px] boder-solid border-[#525960]/20 text-[13px] "
-      >
-        4
-      </button>
-      <button
-        type="button"
-        className="w-[26px] h-[26px] rounded-sm border-[1px] boder-solid border-[#525960]/20 text-[13px] "
-      >
-        5
-      </button>
+      {buttons.map(button => (
+        <button
+          type="button"
+          key={button}
+          onClick={e => onPaginationButtonClickHandler(e, button)}
+          className={`w-[26px] h-[26px] rounded-sm border-[1px] boder-solid border-[#525960]/20 text-[13px] ${
+            selectedIndex === button && 'bg-[#f48225] text-white'
+          }`}
+        >
+          {button + 1}
+        </button>
+      ))}
     </div>
   );
 }
