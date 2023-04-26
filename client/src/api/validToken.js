2b1aaf889502b151dcca3f2f55daec23d74b775b
938938 <938kjh@gmail.com>
2023-04-27 04:22:22 +0900

    🚑[Fix]:validToken 오류 수정

--- client/src/api/validToken.js
+++ client/src/api/validToken.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const fetchLogin = createAsyncThunk('token', async () => {
+export const validToken = createAsyncThunk('token', async () => {
   const url = `${BASE_URL}/v1/token`;
   const token = sessionStorage.getItem('token');
   const response = await axios.get(url, {
--- client/src/store/loginSlice.js
+++ client/src/store/loginSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import validToken from '../api/validToken';
+import { validToken } from '../api/validToken';
 
 // * : email, pw 등의 정보는 여기 저장하지 않고 로그인 유무만 다루는 것에 대해서.
 const loginSlice = createSlice({
