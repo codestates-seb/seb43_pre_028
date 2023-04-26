746241f8f70e47cb8318da840f3242a842b1d795
rlwjd31 <rlwjd31@naver.com>
2023-04-26 20:28:15 +0900

    💄[Add]: 로그인 유무에 따른 header link 변환

--- client/src/components/layout/header/Header.jsx
+++ client/src/components/layout/header/Header.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { StackoverFlowLogo } from '../../Icons';
 
@@ -7,8 +6,6 @@ import Search from './Search';
 import LoginButtons from './LoginButtons';
 
 function Header() {
-  const [foucused, setFocused] = useState(false);
-
   return (
     <header className="flex fixed z-10 w-screen bg-[#F8F8F8] top-0 left-0 border-solid border-t-[3px] border-[#ef8236] shadow-md text-[13px] text-nav-color font-normal">
       <div className="flex items-center justify-center w-[1265px] max-w-[1265px] mx-auto">
