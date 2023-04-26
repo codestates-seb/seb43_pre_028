3286d1daf9d6988e441dd944df507f73f4db8743
mmmkb1 <mmmkb1@naver.com>
2023-04-26 12:56:32 +0900

    �[Feat]: useParams 적용 완료

--- client/src/components/questionDetail/QuestionDetail.jsx
+++ client/src/components/questionDetail/QuestionDetail.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import FadeLoader from 'react-spinners/FadeLoader';
+import { useParams } from 'react-router-dom';
 
 import { selectQuestion } from '../../store/questionDetailSlice';
 import { fetchQuestion } from '../../api/questionDetail';
@@ -11,17 +12,22 @@ import AnswerForm from './component/AnswerForm';
 export default function QuestionDetail() {
   const dispatch = useDispatch();
   const { status, question } = useSelector(selectQuestion);
+  const { questionId } = useParams();
+  console.log(questionId);
 
   useEffect(() => {
     async function fetchQuestionData() {
-      await dispatch(fetchQuestion('123'));
+      await dispatch(fetchQuestion(questionId.toString()));
     }
     fetchQuestionData();
-  }, [dispatch]);
+  }, [dispatch, questionId]);
 
   // 질문과 답변의 상세 페이지를 렌더링하는 컴포넌트
   return (
     <section className="flex w-full flex-col pl-6">
+      {/* <div className="test">
+        <p>현재 페이지의 파라미터는 {questionId} 입니다.</p>
+      </div> */}
       {status === 'loading' && (
         <div className="flex w-full h-[90vh] justify-center items-center bg-red">
           <FadeLoader className="flex w-full bg-red" />
